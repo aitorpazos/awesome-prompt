b3bbff7ad0266fc36fdb323bac7f2ea58472608c
Aitor Pazos <aitor@telnyx.com>
2020-10-27 00:29:32 +0100

    Make the prompt colorful and compact using emojis

--- awesome-prompt.sh
+++ awesome-prompt.sh
@@ -3,23 +3,25 @@
 # awesome-prompt.sh
 #
 # This script provides an easy way to enjoy a colorful responsive prompt.
-# It divides the prompt line in three sections: left, center and right. 
+# It divides the prompt line in three sections: left, center and right.
 # If they don't fit, it will move those sections that doesn't fit to the next line.
 #
 # Instructions:
-# - In order to use this script using the default config, you just need to add 
+# - In order to use this script using the default config, you just need to add
 # this line to your $HOME/.bashrc file:
 #                 source <full path to the script>
 # - You can modify shown information using environment variables that you can
 # export before sourcing the line in the previous point or while running for temporary
 # modifications: export <option>=1
 # - Supported options:
-#		- SHOW_BAT_STATUS -> Shows the battery charge and if you are running on AC or battery
+#       - SHOW_BAT_STATUS -> Shows the battery charge and if you are running on AC or battery
 #       - SHOW_SYS_STATS  -> Shows the most CPU consuming process at the moment
-#       - SHOW_QEMU -> Shows currently running Qemu VMs. Beware of it's performance impact.
-#       - SHOW_VBOX -> Shows currently running VirtualBox VMs. Beware of it's performance impact.
-#       - SHOW_GIT  -> Shows GIT information of current working directory. It requires that 
-#                      git-prompt.sh script which is part of Git distribution 
+#       - SHOW_DOCKER     -> Shows currently running Docker containers. Beware of it's performance impact.
+#       - SHOW_LXC        -> Shows currently running LXC containers. Beware of it's performance impact.
+#       - SHOW_QEMU       -> Shows currently running Qemu VMs. Beware of it's performance impact.
+#       - SHOW_VBOX       -> Shows currently running VirtualBox VMs. Beware of it's performance impact.
+#       - SHOW_GIT        -> Shows GIT information of current working directory. It requires that
+#                      git-prompt.sh script which is part of Git distribution
 #                      (possibly at /etc/bash_completion.d/git-prompt.sh ) has been
 #                      sourced before this script. i.e.:
 #                           source $HOME/.git-prompt.sh
@@ -37,14 +39,23 @@ export PROMPT_COMMAND=prompt
 
 # Colors
 color_default="\[\e[0m\]"
-color_orange="\[\e[0;30m\]"
+color_black="\[\e[0;30m\]"
 color_red="\[\e[0;31m\]"
 color_green="\[\e[0;32m\]"
 color_yellow="\[\e[0;33m\]"
-color_blue="\[\e[1;34m\]"
+color_blue="\[\e[0;34m\]"
 color_fucsia="\[\e[0;35m\]"
 color_cyan="\[\e[0;36m\]"
-color_grey="\[\e[0;37m\]"
+color_white="\[\e[0;37m\]"
+# Background colors
+bg_black="\[\e[40m\]"
+bg_red="\[\e[41m\]"
+bg_green="\[\e[42m\]"
+bg_yellow="\[\e[43m\]"
+bg_blue="\[\e[44m\]"
+bg_fucsia="\[\e[45m\]"
+bg_cyan="\[\e[46m\]"
+bg_white="\[\e[47m\]"
 
 # Function that evaluates last command's exit code.
 # It will show OK if it returned 0, it prints the result code and the command line
@@ -53,14 +64,14 @@ color_grey="\[\e[0;37m\]"
 function exitstatus() {
     local MAX_CHARS=30;
 	if [[ "${lastCommandResult}" -eq "0" ]]; then
-		echo "${color_green}OK${color_default}";
+		echo "${color_green}✅${color_default}";
 	else
 		local suffix="";
 		if [[ "`echo ${lastCommand} | wc -m`" -gt ${MAX_CHARS} ]]; then
 			suffix="...";
 		fi;
 		local commandCut="$(echo ${lastCommand} | cut -c 1-${MAX_CHARS})"
-		echo "${color_red}CMD (Exit Code: ${lastCommandResult}): ${commandCut}${suffix}${color_default}";
+		echo "${color_red}❌ ${lastCommandResult} 👉 ${commandCut}${suffix}${color_default}";
 	fi
 }
 
@@ -84,18 +95,23 @@ function screencenter() {
 # Strips color characters from the string in order to allow the correct layout
 # calcultaions
 function stripcolor() {
-	echo -e "$*" | sed 's/\x1b\[[0-9;]*m//g' | sed 's/\\\[//g' | sed 's/\\\]//g';	
+	echo -e "$*" | sed 's/\x1b\[[0-9;]*m//g' | sed 's/\\\[//g' | sed 's/\\\]//g';
+}
+
+# With emojis, characters calculations may need some adjustment
+function char_adjustment() {
+    echo -e "$*" | sed 's/[📂🕵📄📦🐋🔌🔋💻🧮❌👉✅▶⡇]/--/g'
 }
 
 # Returns the count of characters of a string
 function countstr() {
-	echo "$(stripcolor "$*")" | wc -m;
+    echo "$(char_adjustment $(stripcolor "$*"))" | wc -m;
 }
 
 # Returns a new line if the parameter is 0 and $1 number of spaces otherwise
 function newline_spaces() {
 	if [[ "$1" -eq "0" ]]; then
-		echo "\\n>";
+		echo "\\n";
 	else
 		printf "%*s" "$1" " ";
 	fi
@@ -108,7 +124,7 @@ function syssummary() {
     local cpu="$(echo "${guiltyProc}" | cut -f1 -d',')";
     local mem="$(echo "${guiltyProc}" | cut -f2 -d',' | cut -f2- -d' ')";
     local name="$(echo "${guiltyProc}" | cut -f2 -d'.' | cut -f2- -d' ')";
-    echo "${name}{C:${cpu}%-M:${mem}%}";
+    echo "${color_red}${bg_white}💻:${cpu}% 🧮:${mem}%◀${color_white}${bg_red}${name}";
 	printTiming "Performance info timing" $START;
 }
 
@@ -118,33 +134,33 @@ function batStatus() {
 	local SYS_BAT_BASE="/sys/class/power_supply/"
 	local BAT_STR=""
 	if [ 1 -eq `cat ${SYS_BAT_BASE}/AC/online` ]; then
-	  BAT_STR="${color_green}AC:"
+	  BAT_STR="${color_white}${bg_green}🔌"
 	else
-	  BAT_STR="${color_red}Bat:"
+	  BAT_STR="${color_white}${bg_red}🔋"
 	fi
 	local BAT_NO=1
 
 	for bat in `ls ${SYS_BAT_BASE}`; do
 	  # Checking if batteries attributes exist
-      if [ -f "${SYS_BAT_BASE}/${bat}/capacity" ]; then	  
+      if [ -f "${SYS_BAT_BASE}/${bat}/capacity" ]; then
 	    local BAT_NAME=`cat ${SYS_BAT_BASE}/${bat}/model_name`
 		local DISPLAY_BAT_NAME=${BAT_NAME}
 		if [ ${#DISPLAY_BAT_NAME} -gt 7 ]; then
-			DISPLAY_BAT_NAME=${DISPLAY_BAT_NAME:0:5}..	
+			DISPLAY_BAT_NAME=${DISPLAY_BAT_NAME:0:5}..▶
 		else
-			DISPLAY_BAT_NAME=${DISPLAY_BAT_NAME}-
+			DISPLAY_BAT_NAME=${DISPLAY_BAT_NAME}▶
 		fi
 		local BAT_CHARGING=""
   	    local BAT_CHARGE=`cat "${SYS_BAT_BASE}/${bat}/capacity"`%;
 		if [ -f ${SYS_BAT_BASE}/${bat}/capacity_level ] && [ "Full" == `cat "${SYS_BAT_BASE}/${bat}/capacity_level"` ]; then
 			BAT_CHARGE="Full"
 		fi
-		BAT_STR="${BAT_STR} ${DISPLAY_BAT_NAME}${BAT_CHARGING}${BAT_CHARGE}" 
+		BAT_STR="${BAT_STR}${DISPLAY_BAT_NAME}${BAT_CHARGING}${BAT_CHARGE} "
 	    BAT_NO=$((${BAT_NO} + 1))
-	  fi	
+	  fi
 	done;
 	echo "$BAT_STR"
-	printTiming "Performance info timing" $START;
+	printTiming "Battery info timing" $START;
 }
 
 # Returns the status of Docker containers
@@ -154,7 +170,7 @@ function dockerRunning() {
     if [[ "$?" -eq "0" ]]; then
         local dockerOutput="$(echo "$(docker ps | wc -l) - 1" | bc)";
         if [[ "${dockerOutput}" -ne "0" ]]; then
-            dockerOutput="${dockerOutput} {"$(docker ps --format "{{.Names}}")"}";
+            dockerOutput="${dockerOutput} "$(docker ps --format "{{.Names}}")"";
         fi
         echo ${dockerOutput};
     else
@@ -168,11 +184,11 @@ function lxcRunning() {
 	local START=$(date +%s.%N)
     which virsh > /dev/null 2>&1;
     if [[ "$?" -eq "0" ]]; then
-      local vmCount="$(echo \"$(virsh -r -c lxc:/// list --name 2> /dev/null | wc -l) - 1\" | bc)";
+      local vmCount="$(echo "$(virsh -r -c lxc:/// list --name 2> /dev/null | wc -l) - 1" | bc)";
       if [[ "${vmCount}" -ge "0" ]]; then
         local lxcOutput="${vmCount}";
         if [[ "${lxcOutput}" -gt "0" ]]; then
-            lxcOutput="${lxcOutput} {"$(virsh -r -c lxc:/// list --name)"}";
+            lxcOutput="${lxcOutput} "$(virsh -r -c lxc:/// list --name)"";
         fi;
         echo ${lxcOutput};
       else
@@ -191,7 +207,7 @@ function qemuRunning() {
     if [[ "$?" -eq "0" ]]; then
         local qemuOutput="$(echo "$(virsh -r -c qemu:///session list --name | wc -l) - 1" | bc)";
         if [[ "${qemuOutput}" -ne "0" ]]; then
-            qemuOutput="${qemuOutput} {"$(virsh -r -c qemu:///session list --name)"}";
+            qemuOutput="${qemuOutput} "$(virsh -r -c qemu:///session list --name)"";
         fi
         echo ${qemuOutput};
     else
@@ -207,7 +223,7 @@ function vboxRunning() {
     if [[ "$?" -eq "0" ]]; then
         local vboxOutput="$(echo "$(VBoxManage list runningvms | wc -l)" | bc)";
         if [[ "${vboxOutput}" -ne "0" ]]; then
-            vboxOutput="${vboxOutput} {"$(VBoxManage list runningvms | cut -d'"' -f2)"}";
+            vboxOutput="${vboxOutput} "$(VBoxManage list runningvms | cut -d'"' -f2)"";
         fi
         echo ${vboxOutput};
     else
@@ -226,14 +242,14 @@ function prompt_right() {
 	if [ -n "${SHOW_BAT_STATUS}" ]; then
 		batstatus="$(batStatus)"
 	fi
-	echo "${color_green}${jobs}${color_blue}${containersAndVms}${batstatus} ${color_red}${sysstats} ${color_cyan}$(date +%H:%M:%S)${color_default}";
+	echo "${color_green}${bg_white}${jobs}${color_blue}${bg_white}${containersAndVms}${batstatus}${sysstats} ${color_blue}${bg_white}${color_default}${bg_blue} $(date +%H:%M:%S)${color_default}";
 	printTiming "Right" $START
 }
 
 # Returns the left portion of the prompt
 function prompt_left() {
 	local START=$(date +%s.%N)
-	echo "$USER@$HOST: ${color_fucsia}$(stat -c '%A %U:%G' "$PWD") | D:${dir} (.*:${hiddenDir}) | F:${files} (.*:${hiddenFiles})${color_default}";
+	echo "${bg_blue}$USER@\h${color_blue}${bg_white}▶${color_blue}${bg_white}$(stat -c '%A %U:%G' "$PWD") | 📂${dir} (🕵${hiddenDir}) | 📄${files} (🕵${hiddenFiles})  ${color_default}";
 	printTiming "Left" $START
 }
 
@@ -259,7 +275,7 @@ function right_spaces() {
 	elif [[ "$(countstr "${promptLeftStr}${promptCenterStr}${promptRightStr}")" -gt "$(echo "${COLUMNS} - ${centerSpacesStr}" | bc)" ]]; then
 		# Three fields don't fit
 		spaces="0";
-	else 
+	else
 		spaces="$(echo "${COLUMNS} - ${centerSpacesStr} - $(countstr "${promptLeftStr}${promptCenterStr}${promptRightStr}")" | bc)";
 	fi;
 	echo "${spaces}";
@@ -296,7 +312,7 @@ function prompt() {
     # Saving initial time for logging purposes
     local START=$(date +%s.%N)
 	lastCommand="$(history 1 | cut -f3- -d' ')";
-    
+
     #### Left
     dir="$(find . -maxdepth 1 -type d 2>/dev/null | wc -l)";
     hiddenDir="$(find . -maxdepth 1 -type d -name ".?*" 2>/dev/null | wc -l)";
@@ -311,20 +327,20 @@ function prompt() {
 	printTiming "Center side" $START
 
     #### Right
-    jobs="$(if [[ -n "$(jobs)" ]]; then echo 'J:\j '; else echo ''; fi)";
+    jobs="$(if [[ -n "$(jobs)" ]]; then echo '⚙▶\j '; else echo ''; fi)";
 	#VBox and Qemu are a bit too slow...
 	containersAndVms=""
 	if [ -n "${SHOW_VBOX}" ]; then
-		containersAndVms="${containersAndVms} VBox: $(vboxRunning)";
+		containersAndVms="${containersAndVms} 📦▶$(vboxRunning)";
 	fi
 	if [ -n "${SHOW_QEMU}" ]; then
-		containersAndVms="${containersAndVms} Qemu: $(qemuRunning)";
+		containersAndVms="${containersAndVms} Qemu▶$(qemuRunning)";
 	fi
     if [ -n "${SHOW_LXC}" ]; then
-    	containersAndVms="${containersAndVms} Lxc: $(lxcRunning)";
+    	containersAndVms="${containersAndVms} Lxc▶$(lxcRunning)";
     fi
     if [ -n "${SHOW_DOCKER}" ]; then
-    	containersAndVms="${containersAndVms} Docker: $(dockerRunning)";
+    	containersAndVms="${containersAndVms} 🐋▶$(dockerRunning)";
     fi
     containersAndVms="${containersAndVms} "
 
@@ -338,9 +354,9 @@ function prompt() {
     #### Second line
     local GIT_OUTPUT=""
     if [ -n "${SHOW_GIT}" ]; then
-        GIT_OUTPUT=$(__git_ps1 " (%s)")
+        GIT_OUTPUT=$(__git_ps1 "⎇ %s i")
     fi
-	local lineTwo="${color_blue}"$PWD"${color_green}${GIT_OUTPUT}${color_default}\\$ ";
+	local lineTwo="${color_blue}${bg_white}"$PWD"${color_blue}${bg_yellow}${GIT_OUTPUT}${color_white}${bg_black}▶${color_white}";
 
 	PS1=$(echo -e "${promptLeftStr}$(newline_spaces "${centerSpacesStr}")${promptCenterStr}$(newline_spaces "$(right_spaces)")${promptRightStr}\n${lineTwo}");
 	printTiming "Prompt timing" $START
